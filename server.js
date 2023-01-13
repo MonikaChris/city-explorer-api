d40de09e5144d3aa389694058d441bf1983c703c
Monika <monidavies@gmail.com>
2023-01-12 20:14:52 -0800

    cors

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ const getApiYelp = require('./modules/yelp');
 const app = express();
 
 //Middleware - tells Express app to use cors
-app.use(cors);
+// app.use(cors);
 
-// app.use(cors({
-//   origin: 'https://mcd-city-explorer.netlify.app'
-// }));
+app.use(cors({
+  origin: 'https://mcd-city-explorer.netlify.app'
+}));
 
 
 //Set PORT variable to tell Express app where to serve the server
