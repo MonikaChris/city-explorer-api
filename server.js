ebcf2788ea3f3dc9d3aab0ead5907839ad139721
Monika <monidavies@gmail.com>
2023-07-09 14:16:37 -0700

    Updates Readme

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 // app.use(cors);
 
 app.use(cors({
-  origin: 'https://mcd-city-explorer.netlify.app'
+  origin: ['https://mcd-city-explorer.netlify.app', 'http://localhost:3000']
 }));
 
 
